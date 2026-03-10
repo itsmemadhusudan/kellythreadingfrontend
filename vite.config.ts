0ec86bf17d7cf09ae7f1d8e5a163b5c62541d49c
Madhusudan Timalsina <madhusudantimalsina607@gmail.com>
2026-03-10 14:13:00 +0545

    fix:build error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: false,
+    postcss: {},
   },
   optimizeDeps: {
     include: ['react-is', 'recharts'],
